2b5398c8493337abb7389479c124c2865ae85d53
KingIdBot <77149298+KingIdBot@users.noreply.github.com>
2021-02-04 11:28:32 +0800

    Add files via upload

---
+++ Fxc7/help.js
@@ -0,0 +1,293 @@
+const help = (prefix, instagram, yt, name, pushname2, user, limitt) => { 
+	return `
+
+\`\`\`Hai\`\`\`👋 *${pushname2}*
+*LIMIT KAMU : ${limitt}*
+
+┏━━❖〘 INFORMATION 〙❖━━━⊱
+┣❖ *BOT TYPE : JavaScript*
+┣➲ *NAME : MR KING BOT*
+┣➲ *VERSION NEW : V9.9*
+┣➲ *FREE : Yes*
+┣➲ *VIP : Yes*
+┣➲ *PREMIUM : Yes*
+┣➲ *TOTAL PENGGUNA: ${user.length} User*
+┣➲ *TOTAL DONASI : 1%* 🙂
+┗━━━━━━━━━━━━━━━━━━━━━┛
+
+\`\`\`SUBSCRIBE CHANNEL KAMI\`\`\`
+https://youtube.com/channel/UCHK9gIokua2zkYKYTbqAwMQ
+
+─────────────────┈ 
+ ─➤ *ABOUT*
+─────────────────┈ 
+◆➤ *${prefix}request [Mau Req Fitur Apa?]*
+◆➤ *${prefix}report [lapor bug]*
+◆➤ *${prefix}info*
+◆➤ *${prefix}donasi*
+◆➤ *${prefix}owner*
+◆➤ *${prefix}speed*
+◆➤ *${prefix}daftar*
+◆➤ *${prefix}totaluser*
+◆➤ *${prefix}chatlist*
+◆➤ *${prefix}grouplist*
+◆➤ *${prefix}blocklist*
+◆➤ *${prefix}banlist*
+◆➤ *${prefix}bahasa*
+─────────────────┈ 
+ ─➤ *MEDIA DOWNLOADER* 
+─────────────────┈ 
+◆➤ *${prefix}tiktokstalk [username]*
+◆➤ *${prefix}igstalk [_farhan_xcode7]*
+◆➤ *${prefix}insta [Link]*
+◆➤ *${prefix}instastory [username]*
+◆➤ *${prefix}ssweb [url]*
+◆➤ *${prefix}url2img [Url]*
+◆➤ *${prefix}tiktok*
+◆➤ *${prefix}fototiktok*
+◆➤ *${prefix}meme*
+◆➤ *${prefix}memeindo*
+◆➤ *${prefix}kbbi*
+◆➤ *${prefix}wait*
+◆➤ *${prefix}trendtwit*
+◆➤ *${prefix}google [berita terkini]*
+─────────────────┈ 
+ ─➤ *CREATOR MENU*
+─────────────────┈ 
+◆➤ *${prefix}quotemaker [tx/wtrmk/tema]*
+◆➤ *${prefix}nulis [nama/kelas/text]*
+◆➤ *${prefix}trigger [reply image]*
+◆➤ *${prefix}rip [reply image]*
+◆➤ *${prefix}wasted [reply image]*
+◆➤ *${prefix}cphlogo [MR/BOT]*
+◆➤ *${prefix}cglitch [MR/BOT]*
+◆➤ *${prefix}cpubg [MR/BOT]*
+◆➤ *${prefix}cml [miya/mks]*
+
+◆➤ *${prefix}tahta [MRKING]*
+◆➤ *${prefix}croman [MRKING dan BOT]*
+◆➤ *${prefix}cthunder [MRKING]*
+◆➤ *${prefix}cbpink [MRKING]*
+◆➤ *${prefix}cmwolf [MRKING]*
+◆➤ *${prefix}csky [MRKING]*
+◆➤ *${prefix}cwooden [MRKING]*
+◆➤ *${prefix}cflower [MRKING]*
+◆➤ *${prefix}clove [MRKING]*
+◆➤ *${prefix}ccrossfire [MRKING]*
+◆➤ *${prefix}cnaruto [MRKING]*
+◆➤ *${prefix}cparty [MRKING]*
+◆➤ *${prefix}cshadow [MRKING]*
+◆➤ *${prefix}cminion [MRKING]*
+◆➤ *${prefix}cneon [MRKING]*
+◆➤ *${prefix}cneon2 [MRKING]*
+◆➤ *${prefix}cneongreen [MRKING]*
+◆➤ *${prefix}c3d [MRKING]*
+◆➤ *${prefix}csky [MRKING]*
+
+◆➤ *${prefix}tts [id Halo]*
+◆➤ *${prefix}ttp [MrKing]*
+◆➤ *${prefix}slide [MrKing]*
+◆➤ *${prefix}stiker*
+◆➤ *${prefix}gifstiker*
+◆➤ *${prefix}toimg*
+◆➤ *${prefix}img2url*
+◆➤ *${prefix}nobg*
+◆➤ *${prefix}tomp3*
+◆➤ *${prefix}ocr*
+─────────────────┈ 
+ ─➤ *GROUP ONLY*
+─────────────────┈ 
+◆➤ *${prefix}modeanime [On/Off]*
+◆➤ *${prefix}naruto*
+◆➤ *${prefix}minato*
+◆➤ *${prefix}boruto*
+◆➤ *${prefix}hinata*
+◆➤ *${prefix}sakura*
+◆➤ *${prefix}sasuke*
+◆➤ *${prefix}kaneki*
+◆➤ *${prefix}toukachan*
+◆➤ *${prefix}rize*
+◆➤ *${prefix}akira*
+◆➤ *${prefix}itori*
+◆➤ *${prefix}kurumi*
+◆➤ *${prefix}miku*
+◆➤ *${prefix}anime*
+◆➤ *${prefix}animecry*
+◆➤ *${prefix}neonime*
+◆➤ *${prefix}animekiss*
+◆➤ *${prefix}wink*
+
+◆➤ *${prefix}welcome [On/Off]*
+◆➤ *${prefix}grup [buka/tutup]*
+◆➤ *${prefix}antilink [on/off*
+◆➤ *${prefix}ownergrup*
+◆➤ *${prefix}setpp*
+◆➤ *${prefix}infogc*
+◆➤ *${prefix}add*
+◆➤ *${prefix}kick*
+◆➤ *${prefix}promote*
+◆➤ *${prefix}demote*
+◆➤ *${prefix}setname*
+◆➤ *${prefix}setdesc*
+◆➤ *${prefix}linkgrup*
+◆➤ *${prefix}tagme*
+◆➤ *${prefix}hidetag*
+◆➤ *${prefix}tagall*
+◆➤ *${prefix}mentionall*
+◆➤ *${prefix}fitnah*
+◆➤ *${prefix}listadmin*
+◆➤ *${prefix}openanime*
+◆➤ *${prefix}edotense*
+
+◆➤ *${prefix}nsfw [On/Off]*
+◆➤ *${prefix}nsfwloli*
+◆➤ *${prefix}nsfwblowjob*
+◆➤ *${prefix}nsfwneko*
+◆➤ *${prefix}nsfwtrap*
+◆➤ *${prefix}hentai*
+◆➤ *${prefix}simih [On/Off]*
+─────────────────┈ 
+ ─➤ *FUN*
+─────────────────┈ 
+◆➤ *${prefix}anjing*
+◆➤ *${prefix}kucing*
+◆➤ *${prefix}testime*
+◆➤ *${prefix}hilih*
+◆➤ *${prefix}say*
+◆➤ *${prefix}apakah*
+◆➤ *${prefix}kapankah*
+◆➤ *${prefix}bisakah*
+◆➤ *${prefix}rate*
+◆➤ *${prefix}watak*
+◆➤ *${prefix}hobby*
+◆➤ *${prefix}infogempa*
+◆➤ *${prefix}infonomor*
+◆➤ *${prefix}quotes*
+◆➤ *${prefix}truth*
+◆➤ *${prefix}dare*
+◆➤ *${prefix}katabijak*
+◆➤ *${prefix}fakta*
+◆➤ *${prefix}darkjokes*
+◆➤ *${prefix}bucin*
+◆➤ *${prefix}pantun*
+◆➤ *${prefix}katacinta*
+◆➤ *${prefix}jadwaltvnow*
+◆➤ *${prefix}hekerbucin*
+◆➤ *${prefix}katailham*
+─────────────────┈ 
+ ─➤ *OTHERS* 
+─────────────────┈ 
+◆➤ *${prefix}jarak [Banyuwangi/Surabaya]*
+◆➤ *${prefix}translate [en/Apa kabar?]*
+◆➤ *${prefix}pasangan [Farhan/Iriene]*
+◆➤ *${prefix}gantengcek [Farhan]*
+◆➤ *${prefix}cantikcek [Iriene]*
+◆➤ *${prefix}artinama [Farhan]*
+◆➤ *${prefix}persengay [Topan]*
+◆➤ *${prefix}pbucin [Farhan]*
+◆➤ *${prefix}bpfont [Farhan]*
+◆➤ *${prefix}textstyle [FXC7]*
+◆➤ *${prefix}jadwaltv [antv]*
+◆➤ *${prefix}lirik [melukis senja]*
+◆➤ *${prefix}chord [Melukis senja]*
+◆➤ *${prefix}wiki [Adolf Hitler]*
+◆➤ *${prefix}brainly [pertanyaan]*
+◆➤ *${prefix}resepmasakan [rawon]*
+◆➤ *${prefix}map [Banyuwangi]*
+◆➤ *${prefix}film [Fast and Farious]*
+◆➤ *${prefix}pinterest [gambar kucing]*
+◆➤ *${prefix}infocuaca [Banyuwangi]*
+◆➤ *${prefix}jamdunia [Banyuwangi]*
+◆➤ *${prefix}mimpi [Ular]*
+◆➤ *${prefix}infoalamat [jalan Banyuwangi]*
+
+◆➤  *${prefix}becrypt [string]*
+◆➤  *${prefix}encode64 [string]*
+◆➤  *${prefix}decode64 [encrypt]*
+◆➤  *${prefix}encode32 [string]*
+◆➤  *${prefix}decode32 [encrypt]*
+◆➤  *${prefix}encbinary [string]*
+◆➤  *${prefix}decbinary [encrypt]*
+◆➤  *${prefix}encoctal [string]*
+◆➤  *${prefix}decoctal [encrypt]*
+◆➤  *${prefix}hashidentifier [Encrypt Hash]*
+◆➤  *${prefix}dorking [dork]*
+◆➤  *${prefix}pastebin [teks]*
+◆➤  *${prefix}tinyurl [link]*
+◆➤  *${prefix}bitly [link]*
+─────────────────┈ 
+ ─➤ *GAME* 
+─────────────────┈ 
+◆➤ *${prefix}asupan*
+◆➤ *${prefix}tebakgambar*
+◆➤ *${prefix}caklontong*
+◆➤ *${prefix}family100*
+◆➤ *${prefix}kalkulator [13*12]*
+◆➤ *${prefix}wp [gunung]*
+─────────────────┈ 
+ ─➤ *18+* 
+─────────────────┈ 
+➻ *${prefix}cersex*
+➻ *${prefix}xxx [japan]*
+➻ *${prefix}pornhub [stepMoms]*
+─────────────────┈ 
+ ─➤ *ISLAM* 
+─────────────────┈ 
+➻ *${prefix}jadwalsholat [Banyuwangi]*
+➻ *${prefix}quran*
+➻ *${prefix}quransurah [1]*
+➻ *${prefix}tafsir [1/5]*
+─────────────────┈ 
+ ─➤ *SPAM*
+─────────────────┈ 
+◆➤  *${prefix}spamcall [083xxxxxxxxx]*
+◆➤  *${prefix}spamsms [083xxxxxxxx/jumlah]*
+◆➤  *${prefix}spamgmail [farhanxcode7@gmail.com]*
+─────────────────┈ 
+ ─➤ *OWNER ONLY*
+─────────────────┈ 
+◆➤ *${prefix}addprem [mentioned]*
+◆➤ *${prefix}removeprem [mention]*
+◆➤ *${prefix}setppbot*
+◆➤ *${prefix}setreply*
+◆➤ *${prefix}bc*
+◆➤ *${prefix}bcgc*
+◆➤ *${prefix}ban*
+◆➤ *${prefix}unban*
+◆➤ *${prefix}block*
+◆➤ *${prefix}unblock*
+◆➤ *${prefix}clearall*
+◆➤ *${prefix}delete*
+◆➤ *${prefix}clone*
+◆➤ *${prefix}getses*
+◆➤ *${prefix}leave*
+─────────────────┈
+  ─➤ *PREMIUM ONLY*
+─────────────────┈ 
+◆➤ *${prefix}playmp3 [menepi]*
+◆➤ *${prefix}fb [link video]*
+◆➤ *${prefix}snack [link snack video]*
+◆➤ *${prefix}ytmp3 [link yt]*
+◆➤ *${prefix}ytmp4 [link yt]*
+◆➤ *${prefix}joox [Monolog Pamungkas]*
+◆➤ *${prefix}smule [Link Video Smule]*
+
+╭─────「 *SUPPORT* 」
+│
+├➲ *O BOT*
+├➲ *M. HADI FIRMANSYA*
+├➲ *DELIA AULIA*
+├➲ *KEVIN DAVID*
+├➲ *MR KING*
+├➲ *ORANG MISTERIUS*
+├➲ *TANPA NAMA*
+├➲ *DAN LAIN - LAIN*
+╰────────────────────────
+
+
+          ║▌│█║▌│ █║▌│█│║▌║
+          ║▌│█║▌│ █║▌│█│║▌║
+   
+                 ©️ *MrKing*`
+}
+exports.help = help
